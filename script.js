5ce9e6040ccfad21896d439e93029e40eae063f9
Elizabeth John <117629085+lizannajohn@users.noreply.github.com>
2023-02-28 17:10:35 +0400

    calculate total

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+// const getSumBtn = document.createElement("button");
+// getSumBtn.append("Get Total Price");
+// document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-//Add your code here
+// const getSum = () => {
+// //Add your code here
   
-};
+// };
 
-getSumBtn.addEventListener("click", getSum);
+// getSumBtn.addEventListener("click", getSum);
 
+const calcTotalBtn = document.getElementById("calcTotal");
+calcTotalBtn.addEventListener("click", calcTotal);
+
+function calcTotal()
+{
+    const prices = document.querySelectorAll(".price");
+    let totalPrice = 0;
+    prices.forEach(price =>
+    {
+        totalPrice += parseFloat(price.textContent);
+    });
+
+    const totalPriceCell = document.getElementById("ans");
+    totalPriceCell.textContent = totalPrice.toFixed(2);
+}
